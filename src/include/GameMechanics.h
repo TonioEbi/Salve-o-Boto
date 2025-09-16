269c0fea6c8b3f30170f7252b86b01d43bef5e4b
Bianca Carrer <biancatordivelli@gmail.com>
2025-09-16 18:47:11 -0300

    Revert "Menu"
    This reverts commit c75a962b358be0fdb15ddadd41f66e3c8b5aabbc.

--- src/include/GameMechanics.h
+++ src/include/GameMechanics.h
@@ -1,11 +1,8 @@
 #pragma once
 #include "Player.h"
 #include "Npc.h"
-#include "Bubble.h"
 #include <stdbool.h>
 
 bool playerNpcCollision(Player* p, Npc* n);
 
 void isCaptured(Player* p, Npc* n);
-
-void playerBubbleInteract(Player* p, Bubble* b);
